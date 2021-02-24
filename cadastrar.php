e89e9a92f400f107a094ad945ef1278e42facbb4
HZN001 <heitorsilvanascimento@hotmail.com>
2021-02-24 19:33:07 -0300

    Aprendendo backend

---
+++ cadastrar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+   <div id="corpo-form-cad">
+     <h1>Cadastrar</h1>
+    <form method="post">
+    <input type="text" placeholder="Nome Completo" maxlength="30">
+    <!--Maxlenght, limita os caracteres para evitar futuros erros-->
+    <input type="text" placeholder="Telefone" maxlength="30">
+        <input type="email" placeholder="Usuário" maxlength="40">
+        <input type="password" placeholder="Senha" maxlength="15">
+        <input type="password" placeholder="Confirmar Senha" maxlength="15">
+        <input type="submit" value="Cadastrar">
+        
+
+
+    </form>
+   </div>
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+   <div id="corpo-form">
+     <h1>Entrar</h1>
+    <form method="post">
+        <input type="email" placeholder="Usuário" name="email">
+        <input type="password" placeholder="Senha" name="senha">
+        <input type="submit" value="ACESSAR">
+        <a href="cadastrar.php">  Ainda não é inscrito? <strong>Cadastre-se</strong>   </a>
+
+
+    </form>
+   </div>
+<?php
+
+
+
+?>
+
+</body>
+</html>
